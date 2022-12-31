bf3efd0e53f31671a540e9839c26c8fee5ab02ab
Yannick HUGUES <59506851+YannHug@users.noreply.github.com>
2022-12-31 11:29:54 +0100

    mise en place du formulaire de saisi de prompt

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Form\PromptType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $form = $this->createForm(PromptType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            dd($data);
+        }
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/PromptType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PromptType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('prompt', TextareaType::class, [
+                'label' => 'Votre Prompt',
+                'attr' => [
+                    'placeholder' => 'Tapez votre texte ici',
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Envoyer',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
